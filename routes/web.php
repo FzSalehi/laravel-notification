953035e18a9995ad0c10d6b660b87ce10efb1dc2
Faraz_Salehi <faraz67fz@gmail.com>
2021-05-24 10:02:52 +0430

    simple sending email via mailtrap has implemented

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Mail\TopicCreated;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Mail;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +16,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    Mail::to('faraz@gmail.com')->send(new TopicCreated);
 });
