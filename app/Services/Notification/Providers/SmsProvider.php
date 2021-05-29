f58d3813652242057bfda70ef123165318b753c1
Faraz_Salehi <faraz67fz@gmail.com>
2021-05-29 13:11:42 +0430

    some changes to  accept user model

--- app/Services/Notification/Providers/SmsProvider.php
+++ app/Services/Notification/Providers/SmsProvider.php
@@ -2,24 +2,25 @@
 
 namespace App\Services\Notification\Providers;
 
+use App\Models\User;
 use App\Services\Notification\Providers\Contracts\Provider;
 use App\Services\Sms\Sms;
 
 class SmsProvider implements Provider
 {
-    private $numbers;
+    private $number;
     private $message;
 
-    public function __construct($numbers,string $message)
+    public function __construct(User $user,string $message)
     {
-        $this->numbers = $numbers;
+        $this->number = $user->phone_number;
         $this->message = $message;
     }
 
     public function send()
     {
         $sms = resolve(Sms::class);
-        $res = $sms->send($this->numbers, $this->message);
+        $res = $sms->send($this->number, $this->message);
         if (!$res) dd($sms->getResponse());
         return $res;
     }
