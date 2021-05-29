68d63e6613c4fbaa5c7ae49b4d56717246e89872
Faraz_Salehi <faraz67fz@gmail.com>
2021-05-29 13:14:52 +0430

    changed factory to test other users easyer

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,8 +25,9 @@ class UserFactory extends Factory
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
+            'phone_number' => '09186043460',
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => md5('12345678'), // password
             'remember_token' => Str::random(10),
         ];
     }
